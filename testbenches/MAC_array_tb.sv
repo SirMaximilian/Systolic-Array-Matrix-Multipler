6ee7a5387d7a93f6f671fdf8b24ba4a15e7947c9
Gilbert Barr <gbarr6622@gmail.com>
2020-01-12 10:01:49 -0800

    Changed file types to SystemVerilog

--- testbenches/MAC_array_tb.sv
+++ testbenches/MAC_array_tb.sv
@@ -11,8 +11,24 @@ module MAC_array_tb;
     MAC_array #(.array_size(3), .data_size(8)) MAC_array_unit(.W_matrix_C(W_C), .A_IN(A_r), .B_IN(B_r), .clk(clk), .rst(reset));
     //MAC #(.data_size(8)) instance1(.out_A(W_A), .out_B(W_B), .out_C(W_C), .in_A(A_r), .in_B(B_r), .clk(clk), .reset(reset));
     
+	/*
+	A = [2 1 3
+	     3 2 1
+	     1 3 2]
+		 
+	B = [5 4 6
+	     6 5 6
+		 4 6 5]
+    
+	A * B = C
+	
+	C = [28 31 31
+	     31 28 31
+		 31 31 28]	 
+	*/
+	
     initial begin
-        A_r[0] <= 0;A_r[1] <= 0;A_r[2] <= 0;//0
+        A_r[0] <= 0;A_r[1] <= 0;A_r[2] <= 0;//0 start
         B_r[0] <= 0;B_r[1] <= 0;B_r[2] <= 0;
         clk = 1;
         reset = 1;
@@ -32,6 +48,9 @@ module MAC_array_tb;
         #10
         A_r[0] <= 0;A_r[1] <= 0;A_r[2] <= 2;//5
         B_r[0] <= 0;B_r[1] <= 0;B_r[2] <= 5;
+		#10
+		A_r[0] <= 0;A_r[1] <= 0;A_r[2] <= 0;//6 end
+        B_r[0] <= 0;B_r[1] <= 0;B_r[2] <= 0;
     end
     
     always #5 clk = ~clk;
